b7938a2d43c763dad1115af4cf0ee4caf3327578
Cristianx007 <114533549+Cristianx007@users.noreply.github.com>
2022-09-27 20:46:29 -0500

    Add files via upload

---
+++ java/mx/edu/tesoem/isc/g7s21/p17s2120222ljce/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.g7s21.p17s2120222ljce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
